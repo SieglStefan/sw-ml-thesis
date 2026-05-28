42ab2987673bba68f24be0366d024380cd526840
SieglStefan <stefan.siegl02@gmail.com>
2026-05-28 17:28:52 +0200

    Run and documentation of run_training crash test

--- projects/NeuralLongwave/experiments/21_NeuralLinearLongwave_Training/experiment.jl
+++ projects/NeuralLongwave/experiments/21_NeuralLinearLongwave_Training/experiment.jl
@@ -1,8 +1,13 @@
 ### Test file for crashing Julia REPL
 ###
-### Important: First run setup.jl: experiments/setup.jl
+### Important: First run setup.jl: experiments/setup.jl (one layer abouve this .jl)
 ### Uses Lux.apply() in SpeedyWeather.parameterization!() (parameterizations/neural_llw.jl)
 ### Crahes REPL after about ~ 1 hour
+### Crash message:  The terminal process "julia.exe '-i', '--banner=no', '--project=c:\Code\sw-ml-thesis', 
+#                   'c:\Users\stefa\.vscode\extensions\julialang.language-julia-1.215.2\scripts\terminalserver\terminalserver.jl', 
+#                   '\\.\pipe\vsc-jl-repl-a8d0d154-e423-465a-80fe-f42405a298ee', '\\.\pipe\vsc-jl-repldbg-89ee4056-3d6f-45e8-9169-dfb1295a034a', 
+#                   '\\.\pipe\vsc-jl-cr-32767007-8faf-4e64-940a-0bc9d04d3eaf', 'USE_REVISE=true', 'USE_PLOTPANE=true', 'USE_PROGRESS=true', 
+#                   'ENABLE_SHELL_INTEGRATION=true', 'DEBUG_MODE=false', 'PLOTS_DEFAULT_MIME=image/png'" terminated with exit code: -1073741819.
 
 
 
